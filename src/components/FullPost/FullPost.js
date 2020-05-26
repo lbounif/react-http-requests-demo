9e979527ffca5eae415498ab0edc365a973e5945
Bounif Lynda <lynda.bounif@imadrassa.com>
2020-05-26 15:47:50 +0100

    set a default global configuration for Axios

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -11,7 +11,7 @@ class FullPost extends Component {
     componentDidUpdate() {
         if( this.props.id ) {
             if( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)){
-                axios.get("https://jsonplaceholder.typicode.com/posts/"+ this.props.id)
+                axios.get("/posts/"+ this.props.id)
                 .then(response => {
                     this.setState({loadedPost:response.data})
                 })
@@ -19,14 +19,7 @@ class FullPost extends Component {
         }
     }
     deletePostHandler = () => {
-        axios.delete("https://jsonplaceholder.typicode.com/posts/"+ this.props.id)
-            .then(response => {
-                console.log("here in delete")
-                console.log(response)
-            })
-    }
-    deletePostHandler = () => {
-        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+        axios.delete('/posts/' + this.props.id)
             .then(response => {
                 console.log(response);
             });
