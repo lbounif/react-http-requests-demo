28c9ced824370a4301a02bf9de24360cfb9570a4
Bounif Lynda <lynda.bounif@imadrassa.com>
2020-05-26 11:43:00 +0100

    Init project

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+import post from '../../components/Post/Post';
+
+class Blog extends Component {
+    state = {
+        posts : []
+    }
+    componentDidMount(){
+        Axios.get("https://jsonplaceholder.typicode.com/posts")
+        .then(response =>{
+            const posts = response.data.slice(0, 4)
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: "Lynda"
+                }
+            })
+            this.setState({posts: updatedPosts})
+            //console.log("response is: ", response)
+        })
+    }
+    render () {
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title} author={post.author}/>
+        } )
+        return (
+            <div>
+                <section className="Posts">
+                   {posts}
+                </section>
+                <section>
+                    <FullPost />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
